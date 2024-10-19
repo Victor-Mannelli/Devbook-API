774b3cb8f49b55127a927c39274dc2d5a543b5c5
mannelli <imp.mannelli@gmail.com>
2024-10-19 17:43:17 -0300

    fix: HttpJsonResponse not returning response as json

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -13,6 +13,7 @@ func CheckError(err error) {
 }
 
 func HttpJsonResponse(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 
 	if err := json.NewEncoder(w).Encode(data); err != nil {
